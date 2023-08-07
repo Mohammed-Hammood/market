6419cf8fce4f095bab0fcff48c80ea1ba05c362b
Mohammed <iraqiProfessionalDesigner@gmail.com>
2023-08-08 00:02:36 +0700

    added manifest.json and manifest.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
+import { crx } from '@crxjs/vite-plugin'
+import manifest from './manifest.json'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		crx({ manifest }),
+	],
 
 	build: {
 		outDir: path.resolve(__dirname, 'build'),
